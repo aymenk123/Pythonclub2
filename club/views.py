e20a259cfb54966f5c32b86ee171a52f9969bf4a
Aymen <khaleefaaymen@gmail.com>
2022-03-08 23:07:22 -0800

    Detail Commit

--- club/views.py
+++ club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product, PythonType, Review
 
 # Create your views here.
@@ -6,5 +6,9 @@ def index(request):
  return render(request,'club/index.html')
 
 def products(request):
- product_list=Product.objects.all()
- return render(request, 'club/products.html',{'product_list' : product_list})
+    product_list=Product.objects.all()
+    return render(request, 'club/products.html',{'product_list' : product_list})
+
+def ProductDetail(request, id): 
+    product=get_object_or_404(product, pk=id)
+    return render(request, 'club/productdetail.html', {'product' : product})
